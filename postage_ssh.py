08b9bed7b62d499de9036ac4df258df729487d5b
Fabio Viola <fabio.viola@cmcc.it>
2023-01-11 17:25:30 +0100

    Implemented basic script for postage plots

---
+++ postage_ssh.py
@@ -0,0 +1,183 @@
+#!/usr/bin/python3
+
+
+###############################################
+#
+# global reqs
+#
+###############################################
+
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+from numpy import meshgrid
+from numpy import linspace
+import configparser
+import numpy as np
+import traceback
+import datetime
+import xarray
+import numpy
+import math
+import pdb
+import sys
+import os
+
+
+###############################################
+#
+# initial config
+#
+###############################################
+
+appname = "PlotPostageSsh"
+
+
+###############################################
+#
+# main
+#
+###############################################
+
+if __name__ == "__main__":
+
+    ###############################################
+    #
+    # read input parameters
+    #
+    ###############################################    
+    
+    # read config file name
+    configFile = None
+    try:
+        configFile = sys.argv[1]
+    except:
+        print("[ERROR] -- Config file not provided!")
+        sys.exit(1)
+
+    # read date
+    inputDate = None
+    try:
+        inputDate = sys.argv[2]
+    except:
+        inputDate = datetime.datetime.today().strftime("%Y%m%d")
+
+        
+    ###############################################
+    #
+    # parse config file
+    #
+    ###############################################
+        
+    configParser = configparser.ConfigParser()
+    configParser.read(configFile)
+
+    # paths
+    baseEnsPathTemplate = configParser.get("default", "baseEnsPath")
+    inputFileTemplate = configParser.get("postcardSsh", "inputFile")
+    inputFiles = []
+    print("[%s] -- Input files set to:" % (appname))
+    for i in range(10):
+        inputFile = os.path.join(baseEnsPathTemplate.format(INSTANCE=i, DATE=inputDate), inputFileTemplate.format(DATE=inputDate))
+        inputFiles.append(inputFile)
+        print(inputFile)
+
+    # chart details
+    resolution = configParser.get("postcardSsh", "resolution")
+    colorMap = configParser.get("postcardSsh", "colorMap")
+    minValue = configParser.get("postcardSsh", "minValue")
+    maxValue = configParser.get("postcardSsh", "maxValue")
+    levels = configParser.getint("postcardSsh", "levels")
+    print("[%s] -- Resolution set to: %s" % (appname, resolution))
+    print("[%s] -- Min Value set to: %s" % (appname, minValue))
+    print("[%s] -- Max Value set to: %s" % (appname, maxValue))
+    print("[%s] -- Color map set to: %s" % (appname, colorMap))
+    print("[%s] -- Levels set to: %s" % (appname, levels))
+                    
+        
+    ###############################################
+    #
+    # start processing
+    #
+    ###############################################
+
+    # open files
+    datasets = []
+    for i in inputFiles:
+        datasets.append(xarray.open_dataset(i))
+
+    # grid indices
+    x = datasets[0].nav_lon.transpose().values[0]
+    y = datasets[0].nav_lat.values[0]
+
+    # lats and lons
+    lats = datasets[0].nav_lat.transpose().values[0]
+    lons = datasets[0].nav_lon.values[0]
+
+    # create the grid
+    xxx, yyy = meshgrid(lons, lats)
+    
+    # iterate over timesteps
+    timestep_index = 0
+    for t in datasets[0].time_counter:
+
+        d1 = str(t.values).split("T")[0]        
+        hour = str(t).split("T")[1].split(":")[0]
+        minu = 30
+        d2 = "%s:%s" % (hour, minu)
+        d3 = "%s, %s" % (d1, d2)
+        d4 = "%s_%s%s" % (d1, hour, minu)
+
+        # debug print
+        print("[%s] -- Timestep: %s" % (appname, d3))
+
+        # iterate over depth
+        depth_index = 0
+        for d in datasets[0].deptht:
+        
+            # create a figure
+            fig = plt.figure()
+
+            # title
+            finalDate = "%s:30" % (d3.split(":")[0])
+            plt.title("Salinity at %s m.\nTimestep: %s" % (int(d), finalDate), fontsize = 5, pad = 40)
+            plt.axis('off')
+            
+            # create 10 subfigures
+            subfigures = []
+            for sub_ind in range(10):
+    
+                # create subplot
+                ax = fig.add_subplot(5, 2, sub_ind+1)
+                ax.set_title("Member %s" % sub_ind, fontsize = 4, pad = 4)
+                
+                # create basemap
+                bmap = Basemap(resolution=resolution,
+                               llcrnrlon=lons[0],llcrnrlat=lats[0],
+                               urcrnrlon=lons[-1],urcrnrlat=lats[-1])
+    
+                # contourf
+                mean_data = datasets[sub_ind].vosaline[timestep_index,depth_index,:,:]
+                mean_colormesh = bmap.contourf(xxx, yyy, mean_data, cmap=colorMap, levels=levels)
+                
+                # draw coastlines, country boundaries, fill continents.
+                bmap.drawcoastlines(linewidth=0.25)
+                bmap.fillcontinents(color='white')
+                
+                subfigures.append(ax)
+                
+            # save file
+            di = datasets[0].deptht.values.tolist().index(d)
+            filename = "output/postcard_Salinity_%s_depth%s.png" % (d4, di)
+            plt.savefig(filename, dpi=300, bbox_inches="tight")
+            print("File %s generated" % filename)
+            plt.clf()
+            
+            # increment depth
+            depth_index += 1
+            
+            break
+
+        # increment timestep
+        timestep_index += 1
+            
+        break
