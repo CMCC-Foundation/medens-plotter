e2ae8f2c2071b58be0112d1e848f8b89217dd552
Fabio Viola <fabio.viola@cmcc.it>
2023-01-26 15:11:08 +0100

    Added producePlot.sh script to submit jobs to Zeus

---
+++ producePlots.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# set variables
+APP_NAME="[producePlots] === "
+VARIABLES=("ssh" "temperature" "currents" "salinity")
+CHART_TYPES=("mean_spread" "postage")
+LOG_PATH=/users_home/opa/medens-dev/operationalScripts/logs
+SCRIPT_PATH=/users_home/opa/$USER/operationalScripts/medens-plotter
+CONFIG_FILE="${SCRIPT_PATH}/config_zeus_dev.conf"
+
+# read input params
+# - DATE is the production date
+DATE=$1
+
+source $HOME/.bash_profile
+
+
+##########################################
+#
+# bsub command re-definition
+#
+########################################## 
+
+# redefining bsub
+bsub () {
+    echo bsub $* >&2
+    command bsub $* | head -n1 | cut -d'<' -f2 | cut -d'>' -f1
+}   
+
+# activate conda environment
+source $HOME/.bash_anaconda_3.7
+conda activate /work/opa/medens-dev/.conda/plot
+
+# iterate over the days and over the variables
+for DAY in $(seq 0 8); do
+
+    # debug print
+    echo "$APP_NAME Day $DAY"
+
+    for VAR in ${VARIABLES[*]}; do
+
+	# debug print
+	echo "$APP_NAME === Variable $VAR"
+
+	# iterate over chart types
+	for CTYPE in ${CHART_TYPES[*]}; do
+
+	    # executable
+	    EXE_PATH=$SCRIPT_PATH/${CTYPE}_${VAR}.py
+	    echo -n "Invoking $SCRIPT_PATH/${CTYPE}_${VAR}.py ${CONFIG_FILE} ${DATE} ${DAY}..."
+	    
+	    JOBID=$(bsub -q s_medium -P 0510 -J "plot_${CTYPE}_${VAR}_${DAY}" -o ${LOG_PATH}/plot_${CTYPE}_${VAR}_${DATE}_${DAY}__%J.log -e ${LOG_PATH}/plot_${CTYPE}_${VAR}_${DATE}_${DAY}__%J.err "python $EXE_PATH ${CONFIG_FILE} ${DATE} ${DAY}" &)
+	    echo $JOBID
+	    
+	done	   
+	
+    done
+    
+done
